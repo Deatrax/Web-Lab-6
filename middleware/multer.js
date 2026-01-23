4020635309e25de8cbb1de1a312a392315c5202d
Deatrax <161139217+Deatrax@users.noreply.github.com>
2026-01-23 15:11:55 +0600

    copied ma'am multer code

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,10 +1,26 @@
-const multer = require("multer"); 
+const multer = require("multer");
 
- // write necessary code for storage and file filter
+// write necessary code for storage and file filter
+const storage = multer.diskStorage({ //Stores uploaded files temporarily on your server before sending to Cloudinary
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
 
+const fileFilter = (req, file, cb) => {
+    const allowedTypes = /jpeg|jpg|png|gif/;
+    const mimetype = allowedTypes.test(file.mimetype);
 
+    if (mimetype) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only image files are allowed!"), false);
+    }
+};
 
-const upload = multer({ storage,                                
-                      fileFilter,   
-                      limits: { fileSize: 5 * 1024 * 1024 } }); 
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 }
+});
 module.exports = upload;
