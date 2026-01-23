e00268e382d75bc7df368f993f055d35780d6324
Didhiti0217 <didhiti.nahid@gmail.com>
2026-01-23 15:58:39 +0600

    added 2 controllers

--- controllers/outfitController.js
+++ controllers/outfitController.js
@@ -5,21 +5,115 @@ const Laundry = require('../models/laundryModel');
 const cloudinary = require('../config/cloudinary');
 
 exports.addOutfit = async (req, res) => {
-      // write the code here
+    try {
+        const { name, clothingItems, accessories, weatherCondition } = req.body;
+        
+        let outfitImages = {};
+        
+        // Upload images to Cloudinary if files are provided
+        if (req.files && req.files.length > 0) {
+            const uploadedImage = await cloudinary.uploader.upload(req.files[0].path);
+            outfitImages = {
+                url: uploadedImage.url,
+                public_id: uploadedImage.public_id
+            };
+        }
+        
+        const outfit = new Outfit({
+            name,
+            clothingItems,
+            accessories,
+            weatherCondition,
+            outfitImages
+        });
+        
+        await outfit.save();
+        res.status(201).json(outfit);
+    } catch (err) {
+        res.status(500).json({ message: 'Server Error', error: err.message });
+    }
 };
 
 exports.generateOutfit = async (req, res) => {
-    // write the code here
+    try {
+        const { name, clothingItems, accessories, weatherCondition } = req.body;
+        
+        let outfitImages = {};
+        
+        // Upload images to Cloudinary if files are provided
+        if (req.files && req.files.length > 0) {
+            const uploadedImage = await cloudinary.uploader.upload(req.files[0].path);
+            outfitImages = {
+                url: uploadedImage.url,
+                public_id: uploadedImage.public_id
+            };
+        }
+        
+        const outfit = new Outfit({
+            name,
+            clothingItems,
+            accessories,
+            weatherCondition,
+            outfitImages
+        });
+        
+        await outfit.save();
+        res.status(201).json(outfit);
+    } catch (err) {
+        res.status(500).json({ message: 'Server Error', error: err.message });
+    }
 };
 
 exports.getAllOutfits = async (req, res) => {
-    // write the code here
+    try {
+        const outfits = await Outfit.find()
+            .populate('clothingItems')
+            .populate('accessories');
+        
+        res.status(200).json(outfits);
+    } catch (err) {
+        res.status(500).json({ message: 'Server Error', error: err.message });
+    }
 };
 
 exports.getOutfitById = async (req, res) => {
-    // write the code here
+    try {
+        const { id } = req.params;
+        
+        const outfit = await Outfit.findById(id)
+            .populate('clothingItems')
+            .populate('accessories');
+        
+        if (!outfit) {
+            return res.status(404).json({ message: 'Outfit not found' });
+        }
+        
+        res.status(200).json(outfit);
+    } catch (err) {
+        res.status(500).json({ message: 'Server Error', error: err.message });
+    }
 };
 
 exports.modifyOutfitById = async (req, res) => {
-    // write the code here
+    try {
+        const { id } = req.params;
+        const { name, clothingItems, accessories, weatherCondition } = req.body;
+        
+        const outfit = await Outfit.findById(id);
+        
+        if (!outfit) {
+            return res.status(404).json({ message: 'Outfit not found' });
+        }
+        
+        // Update fields if provided
+        if (name) outfit.name = name;
+        if (clothingItems) outfit.clothingItems = clothingItems;
+        if (accessories) outfit.accessories = accessories;
+        if (weatherCondition) outfit.weatherCondition = weatherCondition;
+        
+        await outfit.save();
+        res.status(200).json(outfit);
+    } catch (err) {
+        res.status(500).json({ message: 'Server Error', error: err.message });
+    }
 };
