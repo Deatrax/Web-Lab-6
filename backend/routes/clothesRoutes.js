2683cb7653e3697ff2a689b99f408434a71c6985
Ridita-A <riditadyuti@gmail.com>
2026-02-28 00:38:30 +0600

    Laundry page added

--- backend/routes/clothesRoutes.js
+++ backend/routes/clothesRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/multer');
 const {
+    getAllClothes,
     addClothes
 } = require('../controllers/clothesController');
 
+router.get('/', getAllClothes);
 router.post('/', upload.array('images', 5), addClothes);
 
 module.exports = router;
--- backend/routes/laundryRoutes.js
+++ backend/routes/laundryRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const {
+    getAllLaundry,
     addLaundry,
-    updateLaundry
+    updateLaundry,
+    deleteLaundry
 } = require('../controllers/laundryController');
 
+router.get('/', getAllLaundry);
 router.post('/', addLaundry);
 router.put('/:id', updateLaundry);
+router.delete('/:id', deleteLaundry);
 
 module.exports = router;
